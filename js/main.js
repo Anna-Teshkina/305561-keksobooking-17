58d6679abf73dfa7fc1f3f5f4e4414e1811ab908
Anna-Teshkina <anna_teshkina@mail.ru>
2019-07-02 17:21:11 +0300

    Page activation

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ var ADVERT_HEIGHT = 70;
 var adverts = []; // массив объявлений
 
 var map = document.querySelector('.map'); // блок карты
-map.classList.remove('map--faded');
 
 var advertList = document.querySelector('.map__pins');
 var advertTemplate = document.querySelector('#pin')
@@ -72,4 +71,69 @@ for (i = 0; i < adverts.length; i++) {
   fragment.appendChild(renderAdvert(adverts[i]));
 }
 
-advertList.appendChild(fragment);
+// При первом открытии, страница находится в неактивном состоянии:
+// блок с картой находится в неактивном состоянии, форма подачи заявления заблокирована.
+
+// элементы управления формы (input, select и т.д.) должны быть неактивны в исходном состоянии
+var formAd = document.querySelector('.ad-form');
+
+var formInput = formAd.querySelectorAll('input');
+for (i = 0; i < formInput.length; i++) {
+  formInput[i].setAttribute('disabled', 'disabled');
+}
+
+var formSelect = formAd.querySelectorAll('select');
+for (i = 0; i < formSelect.length; i++) {
+  formSelect[i].setAttribute('disabled', 'disabled');
+}
+
+var formBtns = formAd.querySelectorAll('button');
+for (i = 0; i < formBtns.length; i++) {
+  formBtns[i].setAttribute('disabled', 'disabled');
+}
+
+// элементы управления формы с фильтрами .map__filters заблокирована так же, как и форма .ad-form;
+var mapFilters = document.querySelector('.map__filters');
+var mapFiltersSelect = mapFilters.querySelectorAll('select');
+for (i = 0; i < mapFiltersSelect.length; i++) {
+  mapFiltersSelect[i].setAttribute('disabled', 'disabled');
+}
+
+var pinMain = map.querySelector('.map__pin--main');
+var widthPinMain = pinMain.offsetWidth; // ширина метки
+var radiusPinMain = widthPinMain / 2; // радиус метки
+
+var leftPinMain = pinMain.offsetLeft; // расстояние от метки до левого края карты
+var topPinMain = pinMain.offsetTop; // расстояние от метки до верха карты
+
+var xPinMain = leftPinMain + radiusPinMain; // начальная координата метки до смещения (ось абсцисс)
+var yPinMain = topPinMain + radiusPinMain; // начальная координата метки до смещения (ось ординат)
+
+
+pinMain.addEventListener('mouseup', function () {
+  // переводим карту в активное состояние
+  map.classList.remove('map--faded');
+
+  // переводим форму и поля формы в активное состояние
+  formAd.classList.remove('ad-form--disabled');
+
+  // в активном состоянии элементы управления формы (input, select и т.д.) должны быть активны
+  for (i = 0; i < formInput.length; i++) {
+    formInput[i].removeAttribute('disabled', 'disabled');
+  }
+
+  for (i = 0; i < formSelect.length; i++) {
+    formSelect[i].removeAttribute('disabled', 'disabled');
+  }
+
+  for (i = 0; i < formBtns.length; i++) {
+    formBtns[i].removeAttribute('disabled', 'disabled');
+  }
+
+  // поле адреса должно быть заполнено, исходное значение поля адреса - середина метки
+  var inputAddress = formAd.querySelector('#address');
+  inputAddress.setAttribute('value', xPinMain + ', ' + yPinMain);
+
+  // выводим метки на страницу
+  advertList.appendChild(fragment);
+});
